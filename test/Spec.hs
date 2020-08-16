2190d63bc96f34b03eb5574ac88db195a806e557
Joaquin Florius <j@florius.com.ar>
2020-08-16 13:57:55 +0000

    Don't auto discovers specs
    (Github actions is having trouble with it)

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,1 +1,16 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+module Spec where
+
+import Test.Hspec
+
+import qualified AppSpec
+import qualified DurationSpec
+import qualified MblSpec
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "App"     AppSpec.spec
+  describe "Duration" DurationSpec.spec
+  describe "Mbl"     MblSpec.spec
